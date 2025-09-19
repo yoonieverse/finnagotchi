81ae79c3a255036c34ea9c87b34dd81e19923b4a
gxl230001 <gxl230001@utdallas.edu>
2025-09-19 18:24:10 -0500

    server test

--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 
 //  fetch user array and then display users
-app.get("/api", (req, res) => {
-    res.json({  "users": ["userOne", "userTwo", "userThree"]})
+app.get("/", (req, res) => {
+    res.json({body: { message: "Hello from server!" }})
 })
 
 
 //  react by default runs 3000
-app.listen(5000, () => { console.log("Server started on port 5000")})
+app.listen(8000, () => { console.log("Server started on port 8000")})
